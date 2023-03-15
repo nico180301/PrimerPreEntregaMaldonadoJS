4493863fdd60f1aff2c64f3663dbbb2063fc19f7
Leonardo Nicolas Maldonado <pirileo2018@gmail.com>
2023-03-15 18:18:53 -0300

    segundo commit

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
-const detallar = (pesocorporal,altura)=>{
-    return "Peso: "+pesocorporal+
-    "\nAltura: "+ altura+"\n"
+const detallar = (pesocorporal, altura) => {
+    return "Peso: " + pesocorporal +
+        "\nAltura: " + altura + "\n"
 }
-const resultado = (detalle,total) => {
-    alert(
-        "Valores Introducidos: \n"+detalle+
-        "======================"+
-        "\nSu Indice de Masa Corporal es: "+total.toFixed(2)
-    )
+const resultado = (detalle, total) => {
+
+    if (total < 18.5 && total >0) {
+        alert(
+            "Valores Introducidos: \n" + detalle +
+            "======================" +
+            "\nSu Indice de Masa Corporal es: " + total.toFixed(2) +
+            "\nSi su IMC es menos de 18.5 se encuentra en un peso insuficiente")
+    } else if (total <= 24.9 && total >= 18.5) {
+        alert(
+            "Valores Introducidos: \n" + detalle +
+            "======================" +
+            "\nSu Indice de Masa Corporal es: " + total.toFixed(2) +
+            "\nSi su IMC es entre 18.5 y 24.9, se encuentra dentro del rango de peso normal o saludable.")
+    } else if (total >= 25.0 && total < 30) {
+        alert(
+            "Valores Introducidos: \n" + detalle +
+            "======================" +
+            "\nSu Indice de Masa Corporal es: " + total.toFixed(2) +
+            "\nSi su IMC es entre 25.0 y 29.9, se encuentra dentro del rango de sobrepeso.")
+    } else{
+        alert(
+            "Valores Introducidos: \n" + detalle +
+            "======================" +
+            "\nSu Indice de Masa Corporal es: " + total.toFixed(2) +
+            "\nSi su IMC es 30.0 o superior, se encuentra dentro del rango de obesidad.")
+    }
 }
 
 let rta = ""
 let total = 0;
 let detalle = ""
-do{
-    let pesocorporal = Number(prompt("Ingrese su peso corporal: "))
-    let altura = Number(prompt("Ingrese su altura, con desimales: "))
-    
-    total += pesocorporal / (altura**2)
-    detalle += detallar(pesocorporal,altura)
+do {
+    pesocorporal = Number(prompt("Ingrese su peso corporal: "))
+    altura = Number(prompt("Ingrese su altura, Ej \"1.70\" : "))
+    console.log(pesocorporal)
+    console.log(altura)
 
-    rta = prompt("Ingrese \"ESC\" para calcular.").toUpperCase()
-}while(rta != "ESC")
+    total += pesocorporal / (altura ** 2)
 
-resultado(detalle,total)
+    detalle += detallar(pesocorporal, altura)
+
+    rta = prompt("Ingrese \"ok\" para calcular.").toUpperCase()
+} while (isNaN(pesocorporal) || isNaN(altura))
+
+resultado(detalle, total)
